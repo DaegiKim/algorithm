6796b1af25564fb1a0e5c08c5597eda90840908b
DaegiKim <rmrhtdms@gmail.com>
2019-03-04 22:00:22 +0900

    solve 9295

---
+++ src/boj/_9295/Main.java
@@ -0,0 +1,14 @@
+package boj._9295;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for(int i=1; i<=N; i++) {
+            System.out.println("Case "+i+": "+ (scanner.nextInt()+scanner.nextInt()));
+        }
+    }
+}

4c2e7ddc2a99b09075ca56dc4d40cd30eb0dd9b2
DaegiKim <rmrhtdms@gmail.com>
2019-06-26 20:30:12 +0900

    solve https://www.hackerrank.com/challenges/kangaroo/problem

---
+++ src/hackerrank/kangaroo/Solution.java
@@ -0,0 +1,57 @@
+package hackerrank.kangaroo;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the kangaroo function below.
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+        if(x1 != x2 && v1 == v2) {
+            return "NO";
+        }
+        else {
+            boolean flag = true;
+
+            while(flag) {
+                if(x1>x2 != x1+v1 > x2+v2) {
+                    return "NO";
+                }
+                else {
+                    x1 += v1;
+                    x2 += v2;
+
+                    if(x1==x2) {
+                        return "YES";
+                    }
+                }
+
+            }
+        }
+
+        return "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        String[] x1V1X2V2 = scanner.nextLine().split(" ");
+
+        int x1 = Integer.parseInt(x1V1X2V2[0]);
+
+        int v1 = Integer.parseInt(x1V1X2V2[1]);
+
+        int x2 = Integer.parseInt(x1V1X2V2[2]);
+
+        int v2 = Integer.parseInt(x1V1X2V2[3]);
+
+        String result = kangaroo(x1, v1, x2, v2);
+
+        scanner.close();
+    }
+}

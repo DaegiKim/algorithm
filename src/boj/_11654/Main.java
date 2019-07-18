9036312609be0299fe4ef40441d6d7ff703025a3
DaegiKim <rmrhtdms@gmail.com>
2019-07-18 15:34:19 +0900

    solve https://www.acmicpc.net/problem/11654

---
+++ src/boj/_11654/Main.java
@@ -0,0 +1,11 @@
+package boj._11654;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
+        System.out.println((int)next.charAt(0));
+    }
+}

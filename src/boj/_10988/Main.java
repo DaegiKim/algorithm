920428704a0df3bd3a3435bca41bbdf5c7a55f68
DaegiKim <rmrhtdms@gmail.com>
2019-07-18 20:57:21 +0900

    solve https://www.acmicpc.net/problem/10988

--- src/boj/_10988/Main.java
+++ src/boj/_10988/Main.java
@@ -6,13 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
+        String reversed = new StringBuilder(str).reverse().toString();
 
-        String head = str.substring(0, str.length()%2!=0 ? (str.length()/2)+1 : str.length()/2);
-        String tail = str.substring(str.length()/2);
-
-        tail = new StringBuilder(tail).reverse().toString();
-
-        if(head.equals(tail)) {
+        if(str.equals(reversed)) {
             System.out.println(1);
         } else {
             System.out.println(0);

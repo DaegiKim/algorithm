573a0861560674b3a1eb638b3dcd0771d86bb79d
DaegiKim <rmrhtdms@gmail.com>
2019-07-25 00:10:55 +0900

    solve https://www.acmicpc.net/problem/2442

--- src/boj/_2442/Main.java
+++ src/boj/_2442/Main.java
@@ -1,16 +1,20 @@
 package boj._2442;
 
-import java.util.Scanner;
+import java.io.*;
 import java.util.stream.IntStream;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
             int length = n-i-1 + (2*(i+1)-1);
-            System.out.printf("%"+length+"s\n", IntStream.range(0, (2*(i+1)-1)).mapToObj(a->"*").reduce(String::concat).orElse(""));
+            bw.write(String.format("%"+length+"s\n", IntStream.range(0, (2*(i+1)-1)).mapToObj(a->"*").reduce(String::concat).orElse("")));
         }
+        bw.flush();
+        bw.close();
     }
 }

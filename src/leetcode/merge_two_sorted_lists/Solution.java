2ca80b919b0989984e71f19be0d5624f6d78781a
DaegiKim <rmrhtdms@gmail.com>
2019-07-26 03:21:19 +0900

    solve https://leetcode.com/problems/merge-two-sorted-lists/

---
+++ src/leetcode/merge_two_sorted_lists/Solution.java
@@ -0,0 +1,42 @@
+package leetcode.merge_two_sorted_lists;
+
+public class Solution {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+
+        Solution solution = new Solution();
+        ListNode listNode = solution.mergeTwoLists(l1, l2);
+
+        System.out.println(listNode);
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+
+        ListNode mergeHead;
+        if(l1.val < l2.val){
+            mergeHead = l1;
+            mergeHead.next = mergeTwoLists(l1.next, l2);
+        }
+        else{
+            mergeHead = l2;
+            mergeHead.next = mergeTwoLists(l1, l2.next);
+        }
+        return mergeHead;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}

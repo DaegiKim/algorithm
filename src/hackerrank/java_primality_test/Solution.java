be5f9e1d091ea8d1ba417d7b8c67adb7550e6846
DaegiKim <rmrhtdms@gmail.com>
2019-07-27 22:37:01 +0900

    solve https://www.hackerrank.com/challenges/java-primality-test

---
+++ src/hackerrank/java_primality_test/Solution.java
@@ -0,0 +1,22 @@
+package hackerrank.java_primality_test;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+//        String n = scanner.nextLine();
+//        scanner.close();
+        try (Scanner scanner = new Scanner(System.in);)
+        {
+            System.out.println(scanner.nextBigInteger().isProbablePrime(100) ? "prime" : "not prime");
+        }
+    }
+}

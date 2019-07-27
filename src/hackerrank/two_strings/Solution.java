2c0e222d35c8392dca093a16276471422fed7020
DaegiKim <rmrhtdms@gmail.com>
2019-07-28 05:06:09 +0900

    solve https://www.hackerrank.com/challenges/two-strings/problem

---
+++ src/hackerrank/two_strings/Solution.java
@@ -0,0 +1,53 @@
+package hackerrank.two_strings;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the twoStrings function below.
+    static String twoStrings(String s1, String s2) {
+        String[] split1 = s1.split("");
+        String[] split2 = s2.split("");
+
+        Set<String> set1 = new HashSet<>(Arrays.asList(split1));
+        Set<String> set2 = new HashSet<>(Arrays.asList(split2));
+
+        for(String s : set1) {
+            if(set2.contains(s)) {
+                return "YES";
+            }
+        }
+
+        return "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}

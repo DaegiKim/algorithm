8fc14f7c204d15ccb36f820e16a0a583baa5cf4c
DaegiKim <rmrhtdms@gmail.com>
2019-08-07 13:03:47 +0900

    solve https://www.acmicpc.net/problem/15740

---
+++ src/boj/_15740/Main.java
@@ -0,0 +1,15 @@
+package boj._15740;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+
+        System.out.println(new BigInteger(split[0]).add(new BigInteger(split[1])).toString());
+    }
+}

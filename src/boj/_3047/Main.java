0e99adec7429ad1f86ee4efd732b9059bf0eec72
DaegiKim <rmrhtdms@gmail.com>
2019-08-10 23:23:27 +0900

    solve https://www.acmicpc.net/problem/3047

--- src/boj/_3047/Main.java
+++ src/boj/_3047/Main.java
@@ -1,18 +1,29 @@
 package boj._3047;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf).sorted().toArray();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[3];
+
+        String[] split = br.readLine().split(" ");
+
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(split[i]);
+        }
+
+        Arrays.sort(arr);
+
         char[] chars = br.readLine().toCharArray();
         for (char aChar : chars) {
-            System.out.print(arr[(int)aChar-65]);
-            System.out.print(" ");
+            bw.write(String.valueOf(arr[(int)aChar-65]));
+            bw.write(' ');
         }
+        bw.flush();
+        bw.close();
     }
 }

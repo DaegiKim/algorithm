838f2d1518d457eb439b2d29b0cd017fd3ab0b1b
DaegiKim <rmrhtdms@gmail.com>
2019-08-11 14:07:54 +0900

    solve https://www.acmicpc.net/problem/5532

--- src/boj/_5532/Main.java
+++ src/boj/_5532/Main.java
@@ -1,20 +1,21 @@
 package boj._5532;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int L = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
 
-        double A = scanner.nextDouble();
-        double B = scanner.nextDouble();
-        double C = scanner.nextDouble();
-        double D = scanner.nextDouble();
+        double A = Double.parseDouble(br.readLine());
+        double B = Double.parseDouble(br.readLine());
+        double C = Double.parseDouble(br.readLine());
+        double D = Double.parseDouble(br.readLine());
 
         int ac = (int) Math.ceil(A/C);
         int bd = (int) Math.ceil(B/D);
-
 
         int max = Math.max(ac,bd);
 

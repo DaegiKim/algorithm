c64fc077dee40f3bebff2be96d282e79107d3dd9
DaegiKim <rmrhtdms@gmail.com>
2019-08-12 20:54:16 +0900

    solve https://www.acmicpc.net/problem/2609

---
+++ src/boj/_2609/Main.java
@@ -0,0 +1,22 @@
+package boj._2609;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+
+        BigInteger a = new BigInteger(split[0]);
+        BigInteger b = new BigInteger(split[1]);
+
+        BigInteger gcd = a.gcd(b);
+        BigInteger lcm = a.divide(gcd).multiply(b.divide(gcd)).multiply(gcd);
+
+        System.out.println(gcd.toString());
+        System.out.println(lcm.toString());
+    }
+}

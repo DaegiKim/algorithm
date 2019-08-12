dabbb6b0552737a2e3601b695a3d1baeca4989a7
DaegiKim <rmrhtdms@gmail.com>
2019-08-12 20:08:29 +0900

    solve https://www.acmicpc.net/problem/9999

---
+++ src/boj/_9999/Main.java
@@ -0,0 +1,9 @@
+package boj._9999;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Arrays.stream("Hymn To Love".split("")).forEach(System.out::print);
+    }
+}
